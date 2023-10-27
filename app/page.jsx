c5bac8ed57fb598cee7797ab373ca791942651be
ArangoMzl <sebastian.arango2411@gmail.com>
2023-10-26 22:10:40 -0500

    Fix to botton theme dark, light or system

--- app/page.jsx
+++ app/page.jsx
@@ -76,9 +76,9 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col justify-between text-center items-center">
+    <div className="flex flex-col justify-between text-center items-center md:m-0 m-2">
       <DropdownMenu>
-        <DropdownMenuTrigger asChild className="absolute right-0 mt-6 mr-10">
+        <DropdownMenuTrigger asChild className="absolute right-0 md:mt-6 md:mr-10 m-auto md:flex hidden">
           <Button variant="outline" size="icon">
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -100,6 +100,27 @@ export default function Home() {
       <div className="flex mt-12">
         <h1 className="font-bold text-4xl"> Mini Blog React + fetch API </h1>
       </div>
+      <br />
+      <DropdownMenu>
+          <DropdownMenuTrigger asChild className=" md:hidden">
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       <br />
       <div>
         <form onSubmit={controladorDelEnvio}>
